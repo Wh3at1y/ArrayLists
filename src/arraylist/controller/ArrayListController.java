176423d708dea93be192a5174ec9302c19eee553
Wh3at1y <smon7958@740s-pl-130.local>
2015-10-07 13:30:43 -0600

    Added view class

--- src/arraylist/controller/ArrayListController.java
+++ src/arraylist/controller/ArrayListController.java
@@ -1,9 +1,19 @@
 package arraylist.controller;
 
+import java.util.ArrayList;
+
 public class ArrayListController
 	{
+		//Everything private
+		private ArrayList<String> firstWords;
+		
+		public ArrayListController()
+		{
+			firstWords = new ArrayList<String>();
+		}
+		
 		public void start()
 		{
-			
+			firstWords.add("keyboard mashing");
 		}
 	}
---
+++ src/arraylist/view/ArrayListDisplay.java
@@ -0,0 +1,11 @@
+package arraylist.view;
+
+import javax.swing.JOptionPane;
+
+public class ArrayListDisplay
+	{
+		public void showList(String currentWords)
+		{
+			JOptionPane.showMessageDialog(null, currentWords);
+		}
+	}
