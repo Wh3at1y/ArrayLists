583e23c944aad9483fa5509e12a3a81664e55d5e
Wh3at1y <smon7958@740s-pl-130.local>
2015-10-07 13:53:10 -0600

    Added a display and for loop

--- src/arraylist/controller/ArrayListController.java
+++ src/arraylist/controller/ArrayListController.java
@@ -2,18 +2,33 @@ package arraylist.controller;
 
 import java.util.ArrayList;
 
+//Class imports
+import arraylist.view.ArrayListDisplay;
+//-----------------
 public class ArrayListController
 	{
 		//Everything private
 		private ArrayList<String> firstWords;
+		private ArrayListDisplay showList;
 		
 		public ArrayListController()
 		{
 			firstWords = new ArrayList<String>();
+			showList = new ArrayListDisplay();
+		}
+		
+		private void displayList()
+		{
+			for(int spot = 0; spot < firstWords.size(); spot++)
+				{
+					showList.showList("The contents at " + spot + " are: " + firstWords.get(spot));
+				}
 		}
 		
 		public void start()
 		{
 			firstWords.add("keyboard mashing");
+			firstWords.add("More keyboard mashing");
+			displayList();
 		}
 	}
